830d6c3a2624557033dccafc07f373c435d8e28c
Brian Bycynski <high5bri@hotmail.com>
2019-12-15 06:35:04 -0500

    finished section 28 - adding authentication

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookiesession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookiesession({
+    keys: ['hdhfg65ekqewl4c7']
+  })
+);
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
     <div>
+    Your ID is: ${req.session.userId}
       <form method='POST'>
         <input name='email' placeholder='email' />
         <input name='password' placeholder='password' />
@@ -18,34 +26,65 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/signup', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use Bozo');
+  }
+
+  if (password !== passwordConfirmation) {
+    res.send('Passwords must match Bozo');
+  }
+  // create a user in our repo to represent this person
+  const user = await usersRepo.create({ email: email, password: password });
+
+  req.session.userId = user.id;
+
   res.send('Account Created');
 });
 
-// const PORT = 3000;
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+    <div>
+      <form method='POST'>
+        <input name='email' placeholder='email' />
+        <input name='password' placeholder='password' />
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email Not Found Bozo');
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+
+  if (!validPassword) {
+    return res.send('Invalid Password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed In!!!!!');
+});
+
 app.listen(3000, () => {
   console.log(`Listening`);
 });
-
-//
-//
-//
-//
-// const bodyParser = (req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.on('data', data => {
-//       const parsed = data.toString('utf8').split('&');
-//       const formData = {};
-
-//       for (let pair of parsed) {
-//         const [key, value] = pair.split('=');
-//         formData[key] = value;
-//       }
-//       req.body = formData;
-//       next();
-//     });
-//   } else {
-//     next();
-//   }
-// };
