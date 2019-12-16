7c2fed917bbda26400112627a42f7bc29be4b084
Brian Bycynski <high5bri@hotmail.com>
2019-12-16 04:03:31 -0500

    finished lecture 390 - working on authentication

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookiesession = require('cookie-session');
-const usersRepo = require('./repositories/users');
+
+const authRouter = require('./routes/admin/auth');
+
+// const usersRepo = require('./repositories/users');
 
 const app = express();
 
@@ -12,78 +15,7 @@ app.use(
   })
 );
 
-app.get('/signup', (req, res) => {
-  res.send(`
-    <div>
-    Your ID is: ${req.session.userId}
-      <form method='POST'>
-        <input name='email' placeholder='email' />
-        <input name='password' placeholder='password' />
-        <input name='passwordConfirmation' placeholder='password confirmation' />
-        <button>Sign Up</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send('Email in use Bozo');
-  }
-
-  if (password !== passwordConfirmation) {
-    res.send('Passwords must match Bozo');
-  }
-  // create a user in our repo to represent this person
-  const user = await usersRepo.create({ email: email, password: password });
-
-  req.session.userId = user.id;
-
-  res.send('Account Created');
-});
-
-app.get('/signout', (req, res) => {
-  req.session = null;
-  res.send('You are logged out');
-});
-
-app.get('/signin', (req, res) => {
-  res.send(`
-    <div>
-      <form method='POST'>
-        <input name='email' placeholder='email' />
-        <input name='password' placeholder='password' />
-        <button>Sign In</button>
-      </form>
-    </div>
-  `);
-});
-
-app.post('/signin', async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await usersRepo.getOneBy({ email });
-
-  if (!user) {
-    return res.send('Email Not Found Bozo');
-  }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-
-  if (!validPassword) {
-    return res.send('Invalid Password');
-  }
-
-  req.session.userId = user.id;
-
-  res.send('You are signed In!!!!!');
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log(`Listening`);
---
+++ views/admin/layout.js
@@ -0,0 +1,15 @@
+//
+//
+//
+module.exports = ({ content }) => {
+  return `
+    <!DOCTYPE>
+    <html>
+      <head>
+      </head>
+      <body>
+        ${content}
+      </body>
+    </html>
+  `;
+};
