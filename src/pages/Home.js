7ebf022132fd75661a411839f0ba87c14a2d3993
Brian Bycynski <high5bri@hotmail.com>
2019-12-24 03:29:50 -0500

    finished lecture 94

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import Services from '../components/Services';
-
+import FeaturedRooms from '../components/FeaturedRooms';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -19,6 +19,7 @@ const Home = () => {
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 };
